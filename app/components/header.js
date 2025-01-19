d055afb61f828f4f7831900d3317118a3cccd3da
annetheblobby <anneliu1999@gmail.com>
2025-01-19 18:45:03 -0500

    resume add ons

--- app/components/header.js
+++ app/components/header.js
@@ -4,16 +4,15 @@ import Link from "next/link";
 export default function Header({ experimentMode = false }) {
   return (
     <div
-      className={`flex gap-5 justify-between w-full whitespace-nowrap text-neutral-500 max-md:flex-wrap max-md:max-w-full py-4 sticky top-0 z-50 ${
-        experimentMode ? `bg-none` : `bg-[#FBF7F7]`
-      } w-[895px]`}
+      className={`flex gap-5 justify-between w-full whitespace-nowrap text-neutral-500 max-md:flex-wrap max-md:max-w-full py-4 sticky top-0 z-50 ${experimentMode ? `bg-none` : `bg-[#FBF7F7]`
+        } w-[895px]`}
     >
       <Link href="/">
         <Logo />
       </Link>
       <div className="flex gap-5 my-auto max-md:flex-wrap max-md:max-w-full major-mono-display-regular">
         <div className="flex flex-auto gap-5 justify-between my-auto max-md:flex-wrap">
-          <Link href="/works" className="font-semibold text-stone-950">
+          <Link href="/" className="font-semibold text-stone-950">
             WORK
           </Link>
           <Link href="/deepDives">DEEP DIVES</Link>
--- app/resume/page.js
+++ app/resume/page.js
@@ -1,3 +1,4 @@
+"use client";
 import * as React from "react";
 import {
   experienceData,
@@ -5,8 +6,41 @@ import {
   educationData,
 } from "../data/resumeData";
 import Header from "../components/header";
+import emailjs from '@emailjs/browser';
+import { useState, useRef } from 'react';
+import CanvasDraw from "react-canvas-draw";
+import { FaUndo } from "react-icons/fa";
+
+
+emailjs.init({
+  publicKey: "Mm4TDs90nDOA5B3O9",
+});
 
 export default function Resume() {
+  const [messageSent, setMessageSent] = useState(false);
+  const [color, setColor] = useState("#000000");
+  const canvasRef = useRef(null);
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    const canvasData = canvasRef.current.canvasContainer.children[1].toDataURL();
+    e.target.elements.drawing.value = canvasData;
+
+    console.log(e.target);
+
+    emailjs.sendForm('service_zjmfecd', 'template_0u66079', e.target)
+      .then((result) => {
+        console.log(result.text);
+        setMessageSent(true);
+        setTimeout(() => setMessageSent(false), 2000);
+        canvasRef.current.clear()
+        e.target.reset();
+      }, (error) => {
+        console.log(error.text);
+      });
+  };
+
   return (
     <main>
       <Header />
@@ -19,12 +53,19 @@ export default function Resume() {
             className="flex flex-col max-w-full w-[1002px]"
             aria-labelledby="about-heading"
           >
-            <h1
-              id="about-heading"
-              className="text-4xl font-medium uppercase text-zinc-800 max-md:max-w-full"
-            >
-              About
-            </h1>
+            <div className="relative">
+              <h1
+                id="about-heading"
+                className="text-4xl font-medium uppercase text-zinc-800 max-md:max-w-full z-10"
+              >
+                About
+              </h1>
+              <svg className="object-contain absolute bottom-px z-[-1] shrink-0 -left-1 self-start" width="149" height="18" viewBox="0 0 149 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M5 12.8534C34.7713 12.8534 63.0757 11.0737 92.6087 7.79231C109.38 5.9288 126.327 5 143.219 5" stroke="#A7EB7B" stroke-width="10" stroke-linecap="round" />
+              </svg>
+
+            </div>
+
             <p className="mt-5 text-2xl tracking-wide text-black max-md:max-w-full">
               I'm a Designer / Developer interested
               <br />
@@ -39,13 +80,6 @@ export default function Resume() {
               aria-labelledby="experience-heading"
             >
               <div className="flex relative flex-col w-full text-3xl font-medium uppercase whitespace-nowrap text-zinc-800 max-md:max-w-full">
-                <img
-                  loading="lazy"
-                  src="https://cdn.builder.io/api/v1/image/assets/TEMP/20343e17857cf351a40102df1ecb99ed8d87b35303ecd9491b9c9950788527fc?placeholderIfAbsent=true&apiKey=db0509071afd4813a2f67c2ecc9770f9"
-                  className="object-contain absolute bottom-0 -left-0.5 z-0 max-w-full h-0 aspect-[23.26] w-[221px]"
-                  alt=""
-                  aria-hidden="true"
-                />
                 <h2 id="experience-heading" className="z-0 max-md:max-w-full">
                   Experience
                 </h2>
@@ -73,13 +107,6 @@ export default function Resume() {
                 className="flex flex-col w-full max-md:max-w-full"
               >
                 <div className="flex relative flex-col w-full text-3xl font-medium uppercase text-zinc-800 max-md:max-w-full">
-                  <img
-                    loading="lazy"
-                    src="https://cdn.builder.io/api/v1/image/assets/TEMP/a575955ee26dcf9c2c2e3673b1cb979940936e092dcc54951fc87ca3090db701?placeholderIfAbsent=true&apiKey=db0509071afd4813a2f67c2ecc9770f9"
-                    className="object-contain absolute bottom-0.5 left-1.5 z-0 max-w-full aspect-[45.45] h-[7px] w-[324px]"
-                    alt=""
-                    aria-hidden="true"
-                  />
                   <h2 id="research-heading" className="z-0 max-md:max-w-full">
                     Research Papers
                   </h2>
@@ -132,6 +159,60 @@ export default function Resume() {
                 </div>
               </section>
             </div>
+
+            <section className="flex flex-col mt-8 w-full max-md:max-w-full">
+              <h2 className="text-3xl font-medium uppercase text-zinc-800">Contact</h2>
+              <form onSubmit={sendEmail} className="flex flex-col mt-5 w-full max-md:max-w-full">
+                <div className="flex flex-row max-md:flex-col gap-4 max-md:max-w-full">
+                  <div className="flex flex-col w-full">
+                    <input
+                      type="text"
+                      name="name"
+                      className="p-2 border border-gray-800"
+                      placeholder="Your name"
+                      required
+                    />
+                    <input
+                      type="email"
+                      name="email"
+                      className="mt-3 p-2 border border-gray-800"
+                      placeholder="Your email"
+                      required
+                    />
+                    <textarea
+                      name="message"
+                      className="p-2 border border-gray-800 mt-3 h-64"
+                      placeholder="Your message"
+                      required
+                    />
+                  </div>
+
+                  <div className="w-full">
+                    <input type="hidden" name="drawing" />
+                    <div className="flex flex-col absolute ml-2 mt-2 bg-gray-100 border border-gray-800 p-2 z-10">
+                      <button type="button" className="p-2 bg-[#A7EB7B] mt-1" onClick={() => setColor("#A7EB7B")}></button>
+                      <button type="button" className="p-2 bg-blue-500 mt-1" onClick={() => setColor("#0000ff")}></button>
+                      <button type="button" className="p-2 bg-black mt-1" onClick={() => setColor("#000000")}></button>
+                      <button type="button" className="pt-1 mt-1" onClick={() => canvasRef.current.undo()}><FaUndo /></button>
+                    </div>
+                    <CanvasDraw
+                      ref={canvasRef}
+                      brushColor={color}
+                      brushRadius={2}
+                      lazyRadius={0}
+                      canvasHeight={368}
+                      className="border border-gray-800 h-64 w-full"
+                    />
+                  </div>
+                </div>
+                <button type="submit" className="mt-3 p-2 bg-[#A7EB7B] text-black w-32">
+                  Send
+                </button>
+                {messageSent && (
+                  <p className="mt-2 font-xs">Message sent successfully!</p>
+                )}
+              </form>
+            </section>
           </div>
         </div>
       </div>
